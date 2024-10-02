68527884c4f165f1b1b92ba78a466f3c56273d39
Katrina Orevillo (She/Her) <58285120+korevillo@users.noreply.github.com>
2024-10-01 23:55:05 -0700

    formatted cards

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <div className='Header'>
         <h1>Adapted Physical Education Assessment Tools</h1>
-        <h2>Quickly find the appropriate assessment tools for your students with disabilities in physical education.</h2>
+        <p>Quickly find the appropriate assessment tools for your students with disabilities in physical education.</p>
     </div>
   );
 }
